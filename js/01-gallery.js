d0a34e73c055ac5cae60651950a5223db8ace972
Yevhen <eug.z2004@gmail.com>
2022-07-30 12:05:07 +0300

    task1 creates markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const createItem = ({ preview, original, description }) => {
+  return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
+
+const createMarkup = (items) => {
+  return items.map(createItem).join("");
+};
+
+const containerRef = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+
+containerRef.insertAdjacentHTML("beforeend", markup);
