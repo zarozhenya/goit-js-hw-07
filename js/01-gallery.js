88addf59ffa91c1dcc52aeaf1db4fefd73ff081a
Yevhen <eug.z2004@gmail.com>
2022-07-30 12:46:05 +0300

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,7 +19,32 @@ const createMarkup = (items) => {
   return items.map(createItem).join("");
 };
 
+/*
+    Creating markup
+*/
+
 const containerRef = document.querySelector(".gallery");
 const markup = createMarkup(galleryItems);
 
 containerRef.insertAdjacentHTML("beforeend", markup);
+
+/*
+    Adding event listener to container
+*/
+
+const onContainerClick = (event) => {
+  event.preventDefault();
+  const { target } = event;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+  const linkRef = target.closest(".gallery__link");
+  const originalImage = linkRef.href;
+
+  const instance = basicLightbox.create(
+    `<img src="${originalImage}" width="1280" />`
+  );
+  instance.show();
+};
+
+containerRef.addEventListener("click", onContainerClick);
